6959fb180a89e75fdecadcd4bec0ca0de0b69f1e
saintpedr0o <prodamakkarz@gmail.com>
2025-09-22 15:40:42 +0300

    remove useless code

--- src/db.py
+++ src/db.py
@@ -8,7 +8,7 @@ from src.config import get_settings
 
 settings = get_settings()
 
-engine = create_async_engine(settings.db_url_str, echo=True)
+engine = create_async_engine(settings.db_url_str)
 
 async_session_factory = async_sessionmaker(bind=engine, expire_on_commit=False)
 
--- src/main.py
+++ src/main.py
@@ -11,8 +11,3 @@ app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 app.include_router(api_router, prefix="/api")
 app.include_router(redirect_router, prefix="/r")
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "src.main:app", host=settings.host, port=settings.port, reload=settings.debug
-    )
